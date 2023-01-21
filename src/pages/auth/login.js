c6ed1847dbfdb29538e3b22e178a0ef388e498b8
karanadam <karanadamv@gmail.com>
2023-01-21 22:25:37 +0530

    added hello world

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
-  return (
-    <div>login</div>
-  )
-}
+  return <div>Hellow world</div>;
+};
 
-export default Login
+export default Login;
